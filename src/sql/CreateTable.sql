ce54586df7cab71eb80b921e241d928d3490dccb
qiezl <qiezl@guanhuai365.com>
2016-04-09 11:15:32 +0800

    -

--- src/sql/CreateTable.sql
+++ src/sql/CreateTable.sql
@@ -1,9 +1,11 @@
-CREATE TABLE [dbo].[Log4NetTest](
-	[dtDate] [datetime] NULL,
-	[sThread] [nvarchar](100) NULL,
-	[SLevel] [nvarchar](200) NULL,
-	[sLogger] [nvarchar](500) NULL,
-	[sMessage] [nvarchar](300) NULL,
-	[sException] [nvarchar](4000) NULL,
+CREATE TABLE [dbo].[Admin_Log](
+	[nId] [bigint] IDENTITY(1,1) NOT NULL,
+	[dtDate] [datetime] NOT NULL,
+	[sThread] [varchar](100) NOT NULL,
+	[sLevel] [varchar](200) NOT NULL,
+	[sLogger] [varchar](500) NOT NULL,
+	[sMessage] [varchar](3000) NOT NULL,
+	[sException] [varchar](4000) NULL,
+	[sIP] [nchar](20) NULL,
 	[sStaffValue] [bigint] NULL
-) ON [PRIMARY]
+) 
